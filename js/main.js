5725bc074a52a8a4987e114eee1d6b8a5e7aaeb8
Hyunjun Kim <hyunjun5216@naver.com>
2021-01-14 22:43:45 +0900

    color variable

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-document.addEventListener('DOMContentLoaded', function(){
-  console.log('script is ready.')
+const toggleBtn = document.querySelector('.navbar__toggleBtn')
+const menu = document.querySelector('.navbar__menu')
+const icons = document.querySelector('.navbar__icons')
+
+toggleBtn.addEventListener('click', () => {
+  menu.classList.toggle('active')
+  icons.classList.toggle('active')
 })
