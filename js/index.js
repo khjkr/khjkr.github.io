743d922ecf0a9c652056ad3e84e122f4c1bf3f19
Hyunjun Kim <hyunjun5216@naver.com>
2021-01-14 22:03:09 +0900

    reset template

--- js/index.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-  console.log('script is ready.')
-});
---
+++ js/main.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function(){
+  console.log('script is ready.')
+})
